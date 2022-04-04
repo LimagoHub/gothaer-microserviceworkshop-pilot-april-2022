f20244d7a50c94488cb554ec0c7e8f07de97639d
Jo Wagner <jowagner@limago.de>
2022-04-04 14:32:28 +0200

    Initial commit

---
+++ smartbank24-kreditantrag-registrierung/src/test/java/de/gothaer/smartbank24kreditantragregistrierung/Smartbank24KreditantragRegistrierungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.gothaer.smartbank24kreditantragregistrierung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Smartbank24KreditantragRegistrierungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
